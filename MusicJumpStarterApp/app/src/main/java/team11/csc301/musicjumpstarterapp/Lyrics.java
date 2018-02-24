f5cb6ab1eeae2647c4cab17f69e7848675a1bc60
Joshua Teichroeb <joshua.teichroeb@mail.utoronto.ca>
2018-02-23 19:36:35 -0500

    empty app

---
+++ MusicJumpStarterApp/app/src/main/java/team11/csc301/musicjumpstarterapp/Lyrics.java
@@ -0,0 +1,13 @@
+package team11.csc301.musicjumpstarterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lyrics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lyrics);
+    }
+}
